fe628d54314aa8c1f53bb3164b94887b4250de96
Danny Grubbs-Donovan <dgrubbsdonovan@middlebury.edu>
2018-12-02 19:24:54 -0500

    Fixes to color mapping

--- Automaton.java
+++ Automaton.java
@@ -1,6 +1,8 @@
 // Implements a Totalistic Cellular Automaton
 
 import java.util.*;
+import java.lang.Math.*;
+import java.awt.Color;
 import java.lang.Integer;
 import java.awt.*;
 
@@ -14,10 +16,10 @@ public class Automaton {
     HashMap<Double,Integer> avgToVal;
 
     //CONSTRUCTOR ==========================================================
-    public Automaton(int kValue, int code) {
-        k = kValue;
+    public Automaton(int kVal, int code) {
+        k = kVal;
         ruleCode = code;
-        kAryRuleCode = intToKAry(ruleCode);
+        kAryRuleCode = intToKAry(ruleCode,this.k);
         //then set other three fields
     };
 
@@ -25,54 +27,125 @@ public class Automaton {
     public int getK() {
         return k;
     };
-    public int getCode() {
-        return ruleCode;
-    };
+
+
     public String[] getKAryCode() {
         return kAryRuleCode;
     };
-    //takes in code and k
-    public String[] intToKAry(int code) {
-    	String[] kCode = new String[(3*k-2)];
+    //converts integer rule code to KAry rule code
+    public static String[] intToKAry(int code, int kVal) {
+    	String[] kCode = new String[(3*kVal-2)];
         for(int n = 0; n < kCode.length; n++) {
-            String digit = Integer.toString((code/((int)Math.pow(k,n)))%k);
+            String digit = Integer.toString((code/((int)Math.pow(kVal,n)))%kVal);
             kCode[kCode.length - n - 1] = digit;
         }
     	return kCode;
      };
-    //public static int KAryToInt(int[] code) {
-      //  return 0;
-    //};
 
     //zeroColor & kColor determine how the rgb values change, will normally
     //just be RGB(255,255,255) & RGB(0,0,0). Also idk how colors work in Java
     //so it prob wonâ€™t actually be of type Color
     //Takes in zeroColor and kColor, which are the bounds of the colors,
     //and returns the color
-    protected Color mapValToColor(Color zeroColor, Color kColor, int val) {
-    	float valFactor = ((float)val/k);
+     public static Color mapValToColor(Color zeroColor, Color kColor, int val, int kInt) {
+    	float valFactor = ((float)val/(float)(kInt));
     	float rangeGreen = Math.abs((float)kColor.getGreen()-(float)zeroColor.getGreen());
     	float rangeBlue = Math.abs((float)kColor.getBlue()-(float)zeroColor.getBlue());
     	float rangeRed = Math.abs((float)kColor.getRed()-(float)zeroColor.getRed());
     	Color tile = new Color((rangeRed*valFactor)/255,(rangeGreen*valFactor)/255,(rangeBlue*valFactor)/255);
     	return tile;
-    };
+    	}
     //this sets the rules
     //sets color value for child based on avg value of parents
     //passes in the avg
-    protected String mapAvgToVal(double avg){
-    	double dif = this.k-avg;
+    public static int mapAvgToVal(double avg,int kInt, String[] kCode){
+    	double dif = kInt-avg;
     	double index = 3*(dif-1);
     	int ind = (int)Math.round(index);
-    	return this.kAryRuleCode[ind];
+    	return Integer.parseInt(kCode[ind]);
     }
 
-    //just for testing
-    //public static void main(String[] args) {
-    //	Automaton a = new Automaton(3,30);
-    //	System.out.println(a.mapValToColor(Color.green,Color.red,1));
-    //	System.out.println(a.intToKAry(32));
-    //	System.out.println(a.mapAvgToVal(.67));
+    // UTILS ===================================================================
+
+    //Create a linked list representing one half of a generation of cells from
+    //genesis cell of past generation
+    public static Cell generate(Cell g, int kInt, String[] kCode) { //later add parameter HashMap rule
+        //determine the value of the c
+        double total = g.state();
+        total += (g.next() != null) ? (2*g.next().state()) : 0; //multiply by 2 for symmetry
+        double avg = total / 3.0;
+        //int center = rule.get(avg)
+        int state = mapAvgToVal(avg,kInt,kCode); //just round for now
+
+        //resursively get left & right by generateNext n other one
+        Cell nextCell = generateNext(g , kInt, kCode);
+
+        return new Cell(state,nextCell);
     }
 
+    //Generate the next cell based on its three parents. If we're at the end of
+    //the list, then base the next cell's state on that of the parent cell in
+    //the direction of the center (lastState)
+    public static Cell generateNext(Cell p, int kInt, String[] kCode) { //later add parameter HashMap rule
+        Cell ret;
+        Cell next;
+        double avg = avgParentStates(p);
+        //int center = rule.get(avg)
+        int center = mapAvgToVal(avg,kInt,kCode); //just round for now
+
+        if(p.next() != null) {
+            next = generateNext(p.next(), kInt, kCode);
+        } else {
+            next = null;
+        }
+
+        return new Cell(center,next);
+    }
+
+    //Avg a cell's parent states, given its parent closest to the center.
+    public static double avgParentStates(Cell c) {
+        double total = 0;
+        if(c != null) {
+            total += c.state();
+            if(c.next() != null) {
+                total += c.next().state();
+                if(c.next().next() != null)
+                total += c.next().next().state();
+            }
+        }
+        return total / 3.0;
+    }
+
+    //utility method to convert generation g into a string
+    public static String g2S(Cell g){
+        String s = "";
+        if(g.next() != null) {
+            s += g2SLeft(g.next()); //Concatenate the cells on the left
+            s += g.state(); //Concatenate genesis cell
+            s += g2SRight(g.next()); //Concatenate the cells on the right
+        } else {
+            s += g.state(); //Just Concatenate genesis cell
+        }
+        return s;
+    }
+
+    //helper methods to g2S
+    public static String g2SLeft(Cell c){
+        String s = "";
+        if(c.next() != null)
+            s += g2SLeft(c.next());
+        s += c.state();
+        return s;
+    }
+
+    public static String g2SRight(Cell c){
+        String s = "";
+        s += c.state();
+        if(c.next() != null)
+            s += g2SRight(c.next());
+        return s;
+    }
+    //public static void main(String[]args) {
+    	//System.out.println(mapValToColor(Color.black,Color.white,0,3));
+    //}
 }
---
+++ AutomatonCanvas.java
@@ -0,0 +1,54 @@
+//Canvas where we will paint our automaton
+
+import java.awt.*;
+
+@SuppressWarnings("serial") // to avoid Eclipse warning
+public class AutomatonCanvas extends Canvas {
+    protected Automata parent;  // access to main applet class
+    protected Automaton a;
+
+    // constructor
+    public AutomatonCanvas(Automata app) {
+        parent = app;
+        a = parent.automaton();
+    }
+
+    // paint the Canvas
+    public void paint ( Graphics g ) {
+        //draw as many gens as we can fit on the screen
+        int cellSize =  parent.zoom();
+        int numOfGens = getWidth()/cellSize/2;
+        drawGeneration(g,new Cell(2),0,0,getWidth()/2,cellSize,0,numOfGens);
+    }
+
+    // Draw a generation gen w/ top at height y, centered at x. n represents the
+    // generation number, starting at 0. Stop drawing gens when genNum is greater
+    // than or equal to numOfGens. Draw each cell w/ edge of size pixels
+    public void drawGeneration( Graphics g, Cell gen, int n, int y, int x, int size, int genNum, int numOfGens) {
+    	//int kInt = a.k;
+    	//String[] kCode = a.kAryRuleCode;
+        g.setColor(a.mapValToColor(Color.black, Color.white, gen.state(), a.k));
+        g.fillRect(x-(size/2),y,size,size);
+        if(gen.next() != null) {
+            drawLeft(g,gen.next(),n,y,x-size,size);
+            drawRight(g,gen.next(),n,y,x+size,size);
+        }
+        if( genNum < numOfGens - 1 )
+            drawGeneration( g, a.generate(gen, a.k,a.kAryRuleCode), n+1, y+size, x, size, genNum+1, numOfGens);
+    }
+
+    public void drawLeft( Graphics g, Cell gen, int n, int y, int x, int size ) {
+    	g.setColor(a.mapValToColor(Color.white, Color.black, gen.state(), a.k));
+    	g.fillRect(x-(size/2),y,size,size);
+        if(gen.next() != null)
+            drawLeft(g,gen.next(),n,y,x-size,size);
+    }
+
+    public void drawRight( Graphics g, Cell gen, int n, int y, int x, int size ) {
+    	g.setColor(a.mapValToColor(Color.white, Color.black, gen.state(), a.k));
+        g.fillRect(x-(size/2),y,size,size);
+        if(gen.next() != null)
+            drawRight(g,gen.next(),n,y,x+size,size);
+    }
+
+}
